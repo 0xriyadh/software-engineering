cdc98e0c28087d808ca49bcbb396590acfdc9edc
Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>
2023-05-17 09:54:30 +0600

    let vs var done

--- javascript/helloWorld.js
+++ javascript/helloWorld.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+let message = "This is in the Global Scope";
+
+{
+    let message = "This is inside a block scope";
+}
+
+function myMessage() { 
+    let message = "This is inside a function scope";
+    return message;
+}
