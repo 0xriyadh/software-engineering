3d8989703b9dc6705bced8604c0719a2c7b1346e
Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>
2023-05-18 15:19:42 +0600

    js hoisting
    Signed-off-by: Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>

--- javascript/helloWorld.js
+++ javascript/helloWorld.js
@@ -1,10 +1,1 @@
-let message = "This is in the Global Scope";
-
-{
-    let message = "This is inside a block scope";
-}
-
-function myMessage() { 
-    let message = "This is inside a function scope";
-    return message;
-}
+console.log("Hello World");
---
+++ javascript/hoisting.js
@@ -0,0 +1,80 @@
+/*
+ * Hoisting
+ * var vs let (and const)
+ * function
+ */
+
+// EXAMPLE 1
+console.log(university);
+university = "BRAC University";
+console.log(university);
+var university;
+/* 
+    let's see what happens when we run this code
+    * var is hoisted
+    * so, the variable university is declared at the top of the code
+    * and the value undefined is assigned to it
+    * so if we run this code, undefined will be printed
+    * and then the value "BRAC University" will be assigned to it
+    * so "BRAC University" will be printed in the next line
+    * We can visualize this process like this:
+        var university;
+        university = undefined
+        console.log(university)
+        university = "BRAC University"
+        console.log(university)
+*/
+/* 
+    instead of var, if we use let, we will get an error
+    * let is hoisted, but not assigned any value
+    * so, if we try to print it, we will get an error. As in js we can't access a variable without assigning a value to it. It remain in a temporal dead zone until it is assigned a value.
+    * some people may think let is not hoisted, but it is. It is just not assigned any value.
+    * we can visualize this process like this:
+        let university;
+        console.log(university)
+        university = "BRAC University"
+        console.log(university)
+    * so we see, like var, let is hoisted, but not assigned any value such as undefined
+*/
+
+// EXAMPLE 2
+var friendsFavFood = "Burger";
+var myFavFood = "Pizza";
+
+function getMyFavFood() {
+    if (!myFavFood) {
+        var myFavFood = friendsFavFood;
+    }
+    return myFavFood;
+}
+
+console.log(`I can eat ${getMyFavFood()} all day long`);
+
+/* 
+    In the above code, our expected result was "I can eat Pizza all day long", However, we got "I can eat Burger all day long". How?
+    * let's visualize the process:
+        var friendsFavFood = "Burger";
+        var myFavFood = "Pizza";
+        function getMyFavFood() {
+            var myFavFood;
+            myFavFood = undefined;
+            if (!myFavFood) {
+                myFavFood = friendsFavFood;
+            }
+            return myFavFood;
+        }
+        console.log(`I can eat ${getMyFavFood()} all day long`);
+    * instead if we used let, we would have got the expected result. 
+    * let's visualize the process:
+        let friendsFavFood = "Burger";
+        let myFavFood = "Pizza";
+        function getMyFavFood() {
+            if (!myFavFood) {
+                let myFavFood;
+                myFavFood = undefined;
+                myFavFood = friendsFavFood;
+            }
+            return myFavFood;
+        }
+    * The reason to all these is let maintain block scope, whereas var maintain function scope. Var gets hoisted to the top of the function, but let gets hoisted to the top of the block.
+*/
