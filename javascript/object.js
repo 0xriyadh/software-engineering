397b1adf1fb52277c497fcdcda098fca8bb99d43
Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>
2023-09-24 12:21:39 +0600

    sync with gitHub
    Signed-off-by: Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>

---
+++ javascript/object.js
@@ -0,0 +1,8 @@
+const myFirstObject = {
+    id: 0,
+    job: " way to store and manage data using keys and values",
+};
+
+const peop = new People("Manush", 12);
+peop.risk = "High";
+console.log(peop);
