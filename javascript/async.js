9c4f76bc3e940b0663079082d11e4a64e66dd49c
Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>
2023-09-28 13:16:40 +0600

    learning js
    Signed-off-by: Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>

---
+++ javascript/async.js
@@ -0,0 +1,36 @@
+const paymentSuccess = true;
+const marks = 80;
+
+function enroll(callback) {
+    console.log("Course enrollment is in progress...");
+
+    setTimeout(function () {
+        if (paymentSuccess) {
+            callback();
+        } else {
+            console.log("Payment failed.");
+        }
+    }, 2000);
+}
+
+function progress(callback) {
+    console.log("Course on progress...");
+
+    setTimeout(function () {
+        if (marks >= 80) {
+            callback();
+        } else {
+            console.log("Course failed.");
+        }
+    }, 3000);
+}
+
+function getCertificate() {
+    console.log("Preparing certificate...");
+
+    setTimeout(() => {
+        console.log("Congratulations! Your certificate is ready.");
+    }, 1000);
+}
+
+enroll(() => progress(getCertificate));
