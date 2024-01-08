e1f908f942ae920249f75445ec8752a147d87cc5
Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>
2024-01-08 09:50:50 +0600

    understanding promise from depth
    Signed-off-by: Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>

---
+++ javascript/promise.js
@@ -0,0 +1,48 @@
+/* 
+    To create a promise, we use the Promise constructor "new Promise". Which takes a callback function with two parameters, "resolve" and "reject". We can write anything else instead of "resolve" and "reject", but it is a convention to use these names.  
+    If the promise is successful or fulfilled, the "resolve" function is called. If the promise is unsuccessful or rejected, the "reject" function is called.
+    And we will do something inside the promise, and then we will call the "resolve" or "reject" function.
+*/
+const hasMeeting = false;
+const meeting = new Promise((resolve, reject) => {
+    // We will do something here
+    if (!hasMeeting) {
+        const meetingDetails = {
+            name: "Technical Meeting",
+            location: "Google Meet",
+            time: "10:00 PM",
+        };
+        resolve(meetingDetails);
+    } else {
+        reject(new Error("Meeting already scheduled!"));
+    }
+});
+
+/* 
+    const addToCalendar = (meetingDetails) => {
+        // We will do something here
+        return new Promise((resolve, reject) => {
+            const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+            resolve(calendar);
+        });
+    }; 
+*/
+// As in the above code we are not using the reject callback. We are sure, our promise will be resolved. We can simplify the code further.
+const addToCalendar = (meetingDetails) => {
+    const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+    return Promise.resolve(calendar);
+};
+
+meeting
+    .then(addToCalendar)
+    .then((res) => {
+        // resolved data
+        // console.log(JSON.stringify(res));
+        console.log(res);
+    })
+    .catch((err) => {
+        // rejected data
+        console.log(err.message);
+    });
+
+console.log("Hello World!"); // This will be printed first, because the promise is asynchronous.
