cb981e77269b85297ae6a7adaaecd8fa2bc9f4c8
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-01-10 09:11:39 +0600

    basic bash done
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- nodejs/express/index.js
+++ nodejs/express/index.js
@@ -3,8 +3,31 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+let patients = [
+    {
+        name: "John",
+        kidneys: [
+            {
+                healthy: false,
+            },
+            {
+                healthy: true,
+            },
+        ],
+    },
+];
+
+const sum = (n) => {
+    let ans = 0;
+    for (let i = 1; i <= n; i++) {
+        ans += i;
+    }
+    return ans;
+};
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    const n = req.query.n;
+    res.send(`Sum of ${n} is ${sum(n)}`);
 });
 
 app.listen(port, () => {
