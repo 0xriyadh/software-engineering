fb20af74499fded11c2294fa773bff8acb0c88b7
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-01-11 09:14:49 +0600

    Basic authentication revision done
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

---
+++ nodejs/express/jwt.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const jwtPassword = "jskdfh234329dfjkb23312sdfs!(*#@&$jksdf324df";
+const users = [
+    {
+        username: "John Doe",
+        password: "1234",
+    },
+    {
+        username: "Radil M",
+        password: "radil23",
+    },
+    {
+        username: "Kipchumba Daniel",
+        password: "kipchumba",
+    },
+];
+
+app.use(express.json());
+
+app.post("/signIn", (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find((user) => {
+        return user.username === username && user.password === password;
+    });
+    if (!user) {
+        res.send("Username or password not in the database");
+    }
+    const accessToken = jwt.sign({ username: user.username }, jwtPassword);
+    res.json({ accessToken });
+});
+
+app.get("/users", (req, res) => { 
+    console.log(req.headers.authorization);
+    const token = req.headers.authorization;
+    try {
+        const decoded = jwt.verify(token, jwtPassword);
+        res.json(users);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
--- nodejs/script.js
+++ nodejs/script.js
@@ -7,15 +7,13 @@ const fetchPosts1 = async () => {
         .then((response) => response.json())
         .then((posts) => console.log("FetchPosts1: ", posts));
 };
-
 // Better Approach
 const fetchPosts2 = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    // response.json() returns a promise. This is used to catch the js
     const posts = await response.json();
     console.log("FetchPosts2: ", posts);
 };
 
 fetchPostsBtn.addEventListener("click", fetchPosts1);
 fetchPostsBtn.addEventListener("click", fetchPosts2);
-
-fetchPosts3();
