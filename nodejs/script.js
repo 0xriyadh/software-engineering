fff7ab21c835a910ab4f113d32182b7762943e8b
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-01-11 07:29:32 +0600

    revised fetch
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

---
+++ nodejs/script.js
@@ -0,0 +1,21 @@
+const fetchPostsBtn = document.getElementById("fetch-post");
+const container = document.getElementById("container");
+
+// Bad Approach (less readable)
+const fetchPosts1 = async () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((posts) => console.log("FetchPosts1: ", posts));
+};
+
+// Better Approach
+const fetchPosts2 = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    console.log("FetchPosts2: ", posts);
+};
+
+fetchPostsBtn.addEventListener("click", fetchPosts1);
+fetchPostsBtn.addEventListener("click", fetchPosts2);
+
+fetchPosts3();
