5071a7cd9eefcfbe240041c116e3e36139bfa85a
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-01-12 14:37:48 +0600

    made the Admin middleware more secure
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- nodejs/express-simple-mongo-jwt-zod-auth/middleware/admin.js
+++ nodejs/express-simple-mongo-jwt-zod-auth/middleware/admin.js
@@ -1,14 +1,22 @@
 const jwt = require("jsonwebtoken");
 
-const adminMiddleware = (req, res, next) => {
+const adminMiddleware = async (req, res, next) => {
     const { authorizationSchemeType, token } =
         req.headers.authorization.split(" ");
-    if (authorizationSchemeType !== "Bearer") { 
+    if (authorizationSchemeType !== "Bearer") {
         return res.status(401).send({ message: "Unauthorized" });
     }
     try {
         const decodedValue = jwt.verify(token, process.env.JWT_SECRET);
-        req.admin = decodedValue;
+
+        // check if the user is an admin
+        const isAdmin = await Admin.findById(decodedValue.id);
+        if (!isAdmin) {
+            return res
+                .status(403)
+                .send({ message: "You are not allowed to visit these pages." });
+        }
+        res.user = decodedValue;
         next();
     } catch (err) {
         res.status(401).send({ message: "Unauthorized" });
--- nodejs/express-simple-mongo-jwt-zod-auth/middleware/user.js
+++ nodejs/express-simple-mongo-jwt-zod-auth/middleware/user.js
@@ -8,7 +8,7 @@ const userMiddleware = async (req, res, next) => {
     }
     try {
         const decodedValue = jwt.verify(token, process.env.JWT_SECRET);
-        req.admin = decodedValue;
+        req.user = decodedValue;
         next();
     } catch (err) {
         res.status(401).send({ message: "Unauthorized" });
