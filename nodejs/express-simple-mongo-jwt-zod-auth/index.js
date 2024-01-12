d1a54c7d0733e4334759ad57bf8e8b009c9b177d
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-01-12 14:56:39 +0600

    admin signup login done
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- nodejs/express-simple-mongo-jwt-zod-auth/index.js
+++ nodejs/express-simple-mongo-jwt-zod-auth/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
+const adminRouter = require("./routes/admin");
 
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 
 // Middleware for parsing JSON and making the data available on req.body
 app.use(express.json());
 
-app.listen(port, () => { 
+// Routes
+app.use("/admin", adminRouter);
+
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
