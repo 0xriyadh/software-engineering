4eec20c669cbe54a69e94c1caec44fe0d710a972
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-11-02 06:37:55 +0600

    practice todo..
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>

--- react/react-again/src/App.jsx
+++ react/react-again/src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import Todo from "./components/Todo";
+
 function App() {
-    return <></>;
+    const [todos, setTodos] = useState([{ id: 1 }]);
+    const handleOnClick = () => {
+        setTodos([...todos, { id: todos.length + 1 }]);
+    };
+    return (
+        <>
+            <h1>All Todos</h1>
+            {/* add todo */}
+            <button onClick={handleOnClick}>Add Todo</button>
+            {/* todo list */}
+            {todos?.map((todo) => (
+                <Todo key={todo.id} num={todo.id} />
+            ))}
+        </>
+    );
 }
 
 export default App;
--- react/react-again/src/components/Todo.jsx
+++ react/react-again/src/components/Todo.jsx
@@ -1,7 +1,23 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+import PropTypes from "prop-types";
 
-const Todo = memo(function Todo() {
-    return <div>Todo</div>;
+const Todo = memo(function Todo(props) {
+    console.log(`Todo rendered with ${props.num}`);
+    const { num } = props;
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h2>
+                Todo App {num}; Count: {count};
+            </h2>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </>
+    );
 });
+
+Todo.propTypes = {
+    num: PropTypes.number.isRequired,
+};
 
 export default Todo;
