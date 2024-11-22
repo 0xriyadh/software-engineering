182d6096b8987d905ced2efdeaf6a9d8d6c5e5fc
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-11-22 10:19:02 +0600

    some updates

--- react/react-again/src/App.jsx
+++ react/react-again/src/App.jsx
@@ -1,11 +1,28 @@
-import ReRender from "./components/ReRender";
-import Todos from "./components/Todos";
+import { useState } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [inputValue, setInputValue] = useState(1);
+
+    console.time("Counting");
+    let count = 0;
+    for (let i = 0; i <= inputValue; i++) {
+        console.log("Counting...");
+        count += i;
+    }
+    console.timeEnd("Counting");
+
     return (
         <>
-            <ReRender />
-            <Todos />
+            <input
+                type="number"
+                onChange={(e) => setInputValue(e.target.value)}
+                value={inputValue}
+            />
+            <p>Sum is {count}</p>
+            <button onClick={() => setCounter(counter + 1)}>
+                Counter ({counter})
+            </button>
         </>
     );
 }
--- react/react-again/src/components/Todo/Todo.jsx
+++ react/react-again/src/components/Todo/Todo.jsx
@@ -5,12 +5,17 @@ const Todo = memo(function Todo(props) {
     console.log(`Todo rendered with ${props.num}`);
     const { num } = props;
     const [count, setCount] = useState(0);
+    const [isTrue, setIsTrue] = useState(true);
 
     return (
         <>
             <h2>
                 Todo App {num}; Count: {count};
             </h2>
+            <p>Is this true? {isTrue}</p>
+            <button onClick={() => setIsTrue((isTrue) => !isTrue)}>
+                Change Is True
+            </button>
             <button onClick={() => setCount(count + 1)}>Increment</button>
         </>
     );
