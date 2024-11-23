bd7c084142da3cbb260cfa8287064cf704863bd5
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-11-23 09:30:27 +0600

    react router dom after a long long time

--- react/react-again/src/App.jsx
+++ react/react-again/src/App.jsx
@@ -1,29 +1,56 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+// import Landing from "./components/Routing/Landing";
+// import Dashboard from "./components/Routing/Dashboard";
+import { lazy, Suspense } from "react";
+
+const Landing = lazy(() => import("./components/Routing/Landing"));
+const Dashboard = lazy(() => import("./components/Routing/Dashboard"));
 
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [inputValue, setInputValue] = useState(1);
-
-    console.time("Counting");
-    let count = 0;
-    for (let i = 0; i <= inputValue; i++) {
-        console.log("Counting...");
-        count += i;
-    }
-    console.timeEnd("Counting");
-
     return (
-        <>
-            <input
-                type="number"
-                onChange={(e) => setInputValue(e.target.value)}
-                value={inputValue}
-            />
-            <p>Sum is {count}</p>
-            <button onClick={() => setCounter(counter + 1)}>
-                Counter ({counter})
-            </button>
-        </>
+        <BrowserRouter>
+            <AppBar />
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route
+                    path="/dashboard"
+                    element={
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Dashboard />
+                        </Suspense>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+function AppBar() {
+    const navigate = useNavigate();
+
+    const handleNavigate = (path) => {
+        navigate(path);
+    };
+    return (
+        <div>
+            <h1 className="text-white text-center py-2 mb-2 bg-red-500">
+                App Bar...
+            </h1>
+            <div className="flex justify-start space-x-4 mx-6">
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => handleNavigate("/")}
+                >
+                    Landing
+                </button>
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => handleNavigate("/dashboard")}
+                >
+                    Dashboard
+                </button>
+            </div>
+        </div>
     );
 }
 
---
+++ react/react-again/src/components/Routing/Landing.jsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">Landing Page...</h1>
+        </div>
+    );
+};
+
+export default Landing;
