9d948bd94d35e4166a1f7ea58a13af306e8a1bd2
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-11-24 23:56:14 +0600

    store done

--- react/simple-todo-recoil/store/todos.jsx
+++ react/simple-todo-recoil/store/todos.jsx
@@ -1,6 +1,38 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
+// state for todo add form
+export const todoFormState = atom({
+    key: "TodoFormState",
+    default: {
+        title: "",
+        description: "",
+    },
+});
+
+// state for todo list
 export const todoListState = atom({
     key: "TodoListState",
     default: [],
+});
+
+// state for todo filter (search)
+export const todoFilterState = atom({
+    key: "TodoFilterState",
+    default: "",
+});
+
+// filtered todo list using recoil selector
+export const filteredTodoListState = selector({
+    key: "FilteredTodoList",
+    get: ({ get }) => {
+        const filter = get(todoFilterState);
+        const list = get(todoListState);
+
+        return list.filter((todo) => {
+            return (
+                todo.title.toLowerCase().includes(filter.toLowerCase()) ||
+                todo.description.toLowerCase().includes(filter.toLowerCase())
+            );
+        });
+    },
 });
