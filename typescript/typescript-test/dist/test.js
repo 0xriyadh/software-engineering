31bb595a27884b656776a5742a9385abde6856d3
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-12-01 21:55:34 +0600

    unn folder removed

--- typescript/typescript-test/dist/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const greetUser = (name) => {
-    console.log(`Hello, ${name}!`);
-};
-function callAfterDelay(callback, delay) {
-    setTimeout(callback, delay);
-}
-callAfterDelay(() => greetUser("Alice"), 1000);
