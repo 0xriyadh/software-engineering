60dc87bb1bf05f03c52e89693fc55c627a172b13
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-12-01 18:47:30 +0600

    custom hook: useInterval

--- react/react-again/src/App.jsx
+++ react/react-again/src/App.jsx
@@ -1,13 +1,16 @@
-import useDimensions from "./hooks/useDimensions.jsx";
+import { useState } from "react";
+import useInterval from "./hooks/useInterval.jsx";
 
 export default function Page() {
-    const dimensions = useDimensions();
+    const [count, setCount] = useState(0);
+
+    useInterval(() => {
+        setCount(count + 1);
+    }, 1000);
 
     return (
         <>
-            <h1>Window Dimensions</h1>
-            <p>Width: {dimensions.width}</p>
-            <p>Height: {dimensions.height}</p>
+            <h1>Timer is at {count}</h1>
         </>
     );
 }
