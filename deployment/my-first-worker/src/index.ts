5021499b590fd60914f26ccb86af97239fcaa9a0
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-12-08 13:21:22 +0600

    cloudflare is awesome

--- deployment/my-first-worker/src/index.ts
+++ deployment/my-first-worker/src/index.ts
@@ -1,18 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env { }
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		/*  
+			When we write a backend app we worry about: 
+				- the method (GET, POST, PUT, DELETE, etc) of the request
+				- the body of the request
+				- the headers of the request
+				- the query parameters of the request
+				etc.
+
+			Here in cloudflare workers we can access all of these things from the `request` object.
+		*/
+		console.log(request.url);
+		console.log(request.method);
+		console.log(request.body);
+		console.log(request.headers);
+
+		if (request.method === 'GET') {
+			return Response.json({
+				message: 'you sent a get request, drogon!!!',
+			});
+		} else {
+			return Response.json({
+				message: 'you did not send a get request',
+			});
+		}
 	},
-} satisfies ExportedHandler<Env>;
+};
