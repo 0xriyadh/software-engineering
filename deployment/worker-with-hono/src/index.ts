b700d29b25a4fd093171bf07ee4c62a0cbf70004
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-12-10 09:02:03 +0600

    hono seems cool too

--- deployment/worker-with-hono/src/index.ts
+++ deployment/worker-with-hono/src/index.ts
@@ -1,9 +1,33 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+import { Context } from "hono";
 
-export default app
+interface Next {
+    (): Promise<void>;
+}
+
+async function authMiddleware(
+    c: Context,
+    next: Next
+): Promise<Response | void> {
+    if (c.req.header("Authorization") !== "Bearer 123") {
+        c.status(401);
+        return c.text("Unauthorized");
+    }
+    await next();
+}
+
+app.use(authMiddleware);
+
+app.get("/", async (c) => {
+    const body = await c.req.json();
+    console.log(body);
+    console.log(c.req.header("Authorization"));
+    console.log(c.req.query("lio"));
+
+    return c.text("Hello Hono!");
+});
+
+export default app;
