856cddc4ff2de6d004e3f51551d1513f7e89e3be
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-12-17 09:35:20 +0600

    maaaaaan... nest is powerfull

--- backend/nest-test-project/src/app.module.ts
+++ backend/nest-test-project/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksController } from './books/books.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule],
+  controllers: [AppController, BooksController],
   providers: [AppService],
 })
 export class AppModule {}
